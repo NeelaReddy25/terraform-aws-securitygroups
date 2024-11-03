ec1f56518a34aad2e9bfbb5264586f6343d935a1
neelareddy <neelareddy.i0204@gmail.com>
2024-11-03 22:03:45 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "project_name" {
 
 variable "environment" {
   type = string
-  default = "dev"
+  #default = "dev"
 }
 
 variable "sg_name" {
@@ -13,4 +13,34 @@ variable "sg_name" {
 
 variable "sg_description" {
   type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "common_tags" {
+  type = map
+}
+
+variable "sg_tags" {
+  type = map
+  default = {}
+}
+
+variable "outbound_rules" {
+  type = list
+  default = {
+    [
+        from_port = 0
+        to_port = 0
+        protocol = "-1" #all protocols
+        cidr_blocks = ["0.0.0.0/0"]
+    ]
+  }
+}
+
+variable "ingress_rules" {
+  type = list
+  default = []
 }
